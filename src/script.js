079988a6fd27e01994d8c9bf4a9e60f6e22035ca
adelina gutu <w1705972@gmail.com>
2025-01-06 23:28:17 +0000

    loaded files optimization

--- src/script.js
+++ src/script.js
@@ -59,7 +59,7 @@ window.addEventListener('resize', () =>
 
 // FLOOR
 
-const floorGeometry = new THREE.PlaneGeometry(10, 10)
+const floorGeometry = new THREE.PlaneGeometry(8, 8)
 const floorMaterial = new THREE.MeshStandardMaterial({
   color: '#ffffff',
   roughness: 0.8,
@@ -67,6 +67,7 @@ const floorMaterial = new THREE.MeshStandardMaterial({
 })
 const floor = new THREE.Mesh(floorGeometry, floorMaterial)
 floor.rotation.x = - Math.PI / 2
+floor.position.set(4,0,4)
 scene.add(floor)
 // )
 
@@ -80,28 +81,12 @@ pointLight.position.set(5, 5, 5)
 scene.add(pointLight)
 
 const pointLightHelper = new THREE.PointLightHelper(pointLight, 1) // POINT LIGHT HELPER
-scene.add(pointLightHelper);
+// scene.add(pointLightHelper);
 
 // BASE
 
 
-// TEXTURES
-const textureLoader = new THREE.TextureLoader()
 
-const darkTexture = textureLoader.load('/textures/matcaps/11.png')
-const lightTexture = textureLoader.load('/textures/matcaps/14.png')
-
-const darkMaterial = new THREE.MeshMatcapMaterial({
-  matcap: darkTexture,
-  // side: THREE.FrontSide,  // Set the side to render (optional, based on your preference)
-  flatShading: false       // Optional: enables flat shading for a more stylized look
-})
-
-const lightMaterial = new THREE.MeshMatcapMaterial({
-  matcap: lightTexture,
-  // side: THREE.FrontSide,
-  flatShading: false
-})
 
 // const parameters = {
 //     color: 0xff0000,
@@ -147,9 +132,8 @@ const lightMaterial = new THREE.MeshMatcapMaterial({
 // )
 
 
-const fbxLoader = new FBXLoader()
 // const loadModelsSequentially = () => {
-// fbxLoader.load(
+  // fbxLoader.load(
 //   '/models/set/fbx/pawn.fbx', // Path to the FBX model
 //   (pawn) => {
 //     console.log('FBX Model loaded successfully!')
@@ -168,45 +152,125 @@ const fbxLoader = new FBXLoader()
 //     console.log(`Loading progress: ${(xhr.loaded / xhr.total) * 100}%`);
 //   },
 //   (error) => {
-//     console.error('Error loading FBX model:', error);
-//   }
-// )
+  //     console.error('Error loading FBX model:', error);
+  //   }
+  // )
 
 
-const loadAndPositionModel = (modelPath, scale, position, material) => {
+
+  // const loadAndPositionModel = (modelPath, scale, position, material) => {
+  //   fbxLoader.load(
+  //     modelPath,
+  //     (object) => {
+  //       console.log(`${modelPath} loaded successfully!`);
+
+  //       // Set the scale
+  //       object.scale.set(scale, scale, scale)
+
+  //       // Traverse and apply material
+  //       object.traverse((child) => {
+  //         if (child.isMesh) {
+  //           // child.geometry.computeVertexNormals()
+  //           child.material = material
+  //         }
+  //       })
+
+  //       // Set the position
+  //       object.position.set(position.x, position.y, position.z)
+
+  //       // Add the object to the scene
+  //       scene.add(object)
+  //     },
+  //     (xhr) => {
+  //       console.log(`Loading progress: ${(xhr.loaded / xhr.total) * 100}%`)
+  //     },
+  //     (error) => {
+  //       console.error(error)
+  //     }
+  //   )
+  // }
+
+
+
+
+// TEXTURES and MATERIALS
+const textureLoader = new THREE.TextureLoader()
+
+const darkTexture = textureLoader.load('/textures/matcaps/11.png')
+const lightTexture = textureLoader.load('/textures/matcaps/14.png')
+
+darkTexture.magFilter  =THREE.NearestFilter
+
+
+const darkMaterial = new THREE.MeshMatcapMaterial({
+  matcap: darkTexture,
+  // side: THREE.FrontSide,  // Set the side to render (optional, based on your preference)
+  flatShading: false       // Optional: enables flat shading for a more stylized look
+})
+
+const lightMaterial = new THREE.MeshMatcapMaterial({
+  matcap: lightTexture,
+  // side: THREE.FrontSide,
+  flatShading: false
+})
+
+// LOADER FBX
+
+const fbxLoader = new FBXLoader()
+
+
+
+// Paths to the models
+const modelPaths = {
+  pawn: '/models/set/fbx/pawn.fbx',
+  bishop: '/models/set/fbx/bishop.fbx',
+  knight: '/models/set/fbx/knight.fbx',
+  rook: '/models/set/fbx/rook.fbx',
+  king: '/models/set/fbx/king.fbx',
+  queen: '/models/set/fbx/queen.fbx'
+}
+
+const loadedModels = {}
+
+const loadModel = (modelName, modelPath, onComplete) => {
   fbxLoader.load(
     modelPath,
     (object) => {
-      console.log(`${modelPath} loaded successfully!`);
-
-      // Set the scale
-      object.scale.set(scale, scale, scale)
-
-      // Traverse and apply material
       object.traverse((child) => {
         if (child.isMesh) {
           // child.geometry.computeVertexNormals()
-          child.material = material
         }
-      });
-
-      // Set the position
-      object.position.set(position.x, position.y, position.z)
-
-      // Add the object to the scene
-      scene.add(object)
+      })
+      loadedModels[modelName] = object
+      if (onComplete) onComplete()
     },
-    (xhr) => {
-      console.log(`Loading progress: ${(xhr.loaded / xhr.total) * 100}%`)
-    },
+    undefined,
     (error) => {
-      console.error('Error loading FBX model:', error)
+      console.error(`Error loading ${modelName}:`, error)
     }
-  );
-};
+  )
+}
 
-// Load each piece and position them
+//  clone and position models
+const addPieceToScene = (modelName, position, material, rotate = false) => {
+  const modelClone = loadedModels[modelName].clone()
+  modelClone.traverse((child) => {
+    if (child.isMesh) {
+      child.material = material
+    }
+  })
+  modelClone.scale.set(scale, scale, scale)
+  modelClone.position.set(position.x, position.y, position.z)
+
+  if (rotate) {
+    modelClone.rotation.z = Math.PI
+  }
+  scene.add(modelClone)
+}
+
 const scale = 0.01
+
+
 const positions = [
   { model: 'pawn', position: { x: 0, y: 0, z: 1 } },
   { model: 'pawn', position: { x: 1, y: 0, z: 1 } },
@@ -226,45 +290,34 @@ const positions = [
   { model: 'rook', position: { x: 7, y: 0, z: 0 } }
 ]
 
-// Paths to the models
-const modelPaths = {
-  pawn: '/models/set/fbx/pawn.fbx',
-  bishop: '/models/set/fbx/bishop.fbx',
-  knight: '/models/set/fbx/knight.fbx',
-  rook: '/models/set/fbx/rook.fbx',
-  king: '/models/set/fbx/king.fbx',
-  queen: '/models/set/fbx/queen.fbx'
-};
 
-// Load each piece at its respective position
+// Load all models
+Object.entries(modelPaths).forEach(([modelName, modelPath]) => {
+  loadModel(modelName, modelPath, () => {
+    if (Object.keys(loadedModels).length === Object.keys(modelPaths).length) {
+      // Center positions dynamically
+      const centeredPosition = (coord) => coord + 0.5
+      // const mirrorCenteredPosition = (coord) => coord - 0.5
 
-positions.forEach(({ model, position }) => {
-  loadAndPositionModel(modelPaths[model], scale, position, darkMaterial);
+
+      // Add first set to the scene
+      positions.forEach(({ model, position }) => {
+        position.x = centeredPosition(position.x)
+        position.z = centeredPosition(position.z)
+
+        addPieceToScene(model, position, darkMaterial, false)
+      })
+
+
+      // Add mirrored set to the scene with light material
+      positions.forEach(({ model, position }) => {
+        const mirroredPosition = { x: 7 - position.x, y: position.y, z: 7 - position.z }
+        addPieceToScene(model, mirroredPosition, lightMaterial, true);
+      })
+
+    }
+  })
 })
-// Load all models once
-// Object.keys(modelPaths).forEach((modelName) => {
-//   loadAndStoreModel(modelPaths[modelName], modelName);
-// })
-
-// clone
-// const loadMirroredModels = () => {
-//   const mirrorPositions = positions.map(({ model, position }) => ({
-//     model,
-//     position: { x: 7 - position.x, y: position.y, z: position.z }
-//   }))
-
-//   // Loop through the mirrored positions and clone the models
-//   mirrorPositions.forEach(({ model, position }) => {
-//     const modelClone = loadedModels[model].clone() // Clone the already loaded model
-//     modelClone.material = lightMaterial // Apply new material
-//     modelClone.position.set(position.x, position.y, position.z) // Set mirrored position
-//     scene.add(modelClone);// Add cloned object to the scene
-//   });
-// };
-
-
-// loadMirroredModels()
-
 
 
 
@@ -280,17 +333,21 @@ positions.forEach(({ model, position }) => {
 
 // CAMERA
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 3
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(55, aspectRatio, 0.1, 100)
 // ortho CAMERA
-// const aspectRatio = sizes.width / sizes.height
 // const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
-
+camera.position.x = 4
+camera.position.y = 4
+camera.position.z = 7
+// camera.lookAt(4, 4, 5)
 scene.add(camera)
 // camera.lookAt(mesh.position)
 
+
+gui.add(camera.position, 'x').min(-3).max(6).step(0.1).name('Position X')
+gui.add(camera.position, 'y').min(-3).max(6).step(0.1).name('Position Y')
+gui.add(camera.position, 'z').min(-3).max(6).step(0.1).name('Position Z')
 
 // gui.hide()
 // gui.add(mesh.position, 'y').min(- 3).max(3).step(0.01).name('elevation')
@@ -330,7 +387,7 @@ controls.dampingFactor = 0.25
 controls.maxPolarAngle = Math.PI / 2
 
 // controls.attach(object)
-scene.add(controls)
+// scene.add(controls)
 
 // -----------------------------------------
 // RENDERER
@@ -339,6 +396,15 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+window.addEventListener('dblclick', () => {
+  if(!document.fullscreenElement) {
+      canvas.requestFullscreen()
+  }
+  else{
+      document.exitFullscreen()
+  }
+})
 
 // -----------------------------------------
 // ANIMATE
